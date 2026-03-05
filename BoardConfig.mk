c1c052fb0e97edc209aed1ce67a84a8f63f48cd8
Abdallah Ibrahim <iabdollah40@gmail.com>
2026-03-05 01:54:05 +0000

    nicky: Update configuration for Android 16 qpr2 support
    - BoardConfig: Enable FBE metadata decryption and add boot_devices to cmdline.
    - fstab: Map metadata to logdump, remove inlinecrypt, and clean up mount flags.
    - flags: Add wipeingui for metadata and mounttodecrypt for persist.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ BOARD_KERNEL_CMDLINE := \
     earlycon=msm_geni_serial,0xA90000 \
     androidboot.hardware=qcom \
     androidboot.console=ttyMSM0 \
+    androidboot.boot_devices=soc/1d84000.ufshc \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     msm_rtb.filter=0x237 \
     ehci-hcd.park=3 \
@@ -109,6 +110,9 @@ TARGET_USES_MKE2FS := true
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
 TW_USE_FSCRYPT_POLICY := 1
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 
